48e17ac1913413ad8c652ba46c0804152d5a9037
Dian Aditya <dian.aditya53@gmail.com>
2022-10-11 04:37:09 +0700

    challenge 4

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
